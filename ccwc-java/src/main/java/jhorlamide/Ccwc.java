ddcde68598a5c0fe6d5c8df20866fce4e554c521
olamide jubril <olamidejubril68@gmail.com>
2023-12-20 10:01:49 +0100

    Refactored the main class

--- ccwc-java/src/main/java/jhorlamide/Ccwc.java
+++ ccwc-java/src/main/java/jhorlamide/Ccwc.java
@@ -1,8 +1,25 @@
 package jhorlamide;
 
-public class Ccwc {
-//    @Override
-//    public String call() throws Exception {
-//        return "Hello, World!";
-//    }
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+
+import java.util.concurrent.Callable;
+
+@Command(name = "ccwc", mixinStandardHelpOptions = true, version = "ccwc 1.0",
+        description = "simple wc tool to counts the number for lines, words, characters from a file")
+public class Ccwc implements Callable<String> {
+    public void run(String[] args) {
+        var ccwc = new Ccwc();
+        var command = new CommandLine(ccwc);
+        var exitCode = command.execute(args);
+        var result = command.getExecutionResult();
+
+        System.out.println(result);
+        System.exit(exitCode);
+    }
+
+    @Override
+    public String call() throws Exception {
+        return "Hello, World!";
+    }
 }
--- ccwc-java/src/main/java/jhorlamide/Main.java
+++ ccwc-java/src/main/java/jhorlamide/Main.java
@@ -1,26 +1,9 @@
 package jhorlamide;
 
-import picocli.CommandLine;
-import picocli.CommandLine.Command;
 
-import java.util.concurrent.Callable;
-
-@Command(name = "ccwc", mixinStandardHelpOptions = true, version = "ccwc 1.0",
-        description = "simple wc tool to counts the number for lines, words, characters from a file")
-public class Main implements Callable<String> {
+public class Main {
     public static void main(String[] args) {
         var ccwc = new Ccwc();
-        var command = new CommandLine(ccwc);
-        var exitCode = command.execute(args);
-        var result = command.getExecutionResult();
-
-
-        System.out.println(result);
-        System.exit(exitCode);
-    }
-
-    @Override
-    public String call() throws Exception {
-        return "Hello, World!";
+        ccwc.run(args);
     }
 }
